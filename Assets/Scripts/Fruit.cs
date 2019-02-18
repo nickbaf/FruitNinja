ea71f1c94cb409c4ff7d7dacdfebd5792b19e07a
nickbaf <nickbaf@icloud.com>
2019-02-18 21:45:12 +0000

    initial

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject slicedFruit;
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ScliceTheFruit();
+        }
+    }
+
+
+    public void ScliceTheFruit()
+    {
+
+        GameObject instance = (GameObject) Instantiate(slicedFruit, transform.position, transform.rotation);
+
+        Rigidbody[] slices = instance.transform.GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody r in slices)
+        {
+            r.transform.rotation = Random.rotation;
+            r.AddExplosionForce(Random.Range(600, 1200), transform.position, 5f);
+        }
+        Destroy(gameObject);
+    }
+
+
+}
