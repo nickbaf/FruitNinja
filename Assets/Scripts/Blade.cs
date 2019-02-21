27a758fab5b05bd02c7488f40d9e227cb66458e4
nickbaf <nickbaf@icloud.com>
2019-02-21 13:32:34 +0000

    minor upgrades

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -7,15 +7,23 @@ public class Blade : MonoBehaviour
 
     private Rigidbody2D b;
 
+    private float minVelocity = 0.1f;
+    private Vector3 lastPos;
+    private Vector3 mouseVel;
+
+    private Collider2D col;
+
     // Start is called before the first frame update
     void Awake()
     {
         b = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        col.enabled = IsMoving();
         followBlade();
     }
 
@@ -26,5 +34,20 @@ public class Blade : MonoBehaviour
         var mousePos = Input.mousePosition;
         mousePos.z = 10;
         b.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+
+
+    private bool IsMoving()
+    {
+        Vector3 mousePos = transform.position;
+        float traveled = (lastPos - mousePos).magnitude;
+        lastPos = mousePos;
+
+        if (traveled > minVelocity)
+        {
+            return true;
+        }
+        return false;
     }
 }
